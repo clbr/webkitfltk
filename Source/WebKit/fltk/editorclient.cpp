147bae346956b1f91201dba74708f743eb74f371
Lauri Kasanen <curaga@operamail.com>
2014-06-25 18:33:11 +0300

    Start on editor

--- Source/WebKit/fltk/editorclient.cpp
+++ Source/WebKit/fltk/editorclient.cpp
@@ -19,10 +19,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "editorclient.h"
 #include "webviewpriv.h"
 
+#include <Editor.h>
 #include <Frame.h>
 #include <FrameSelection.h>
-#include "NotImplemented.h"
+#include <KeyboardEvent.h>
+#include <NotImplemented.h>
+#include <PlatformKeyboardEvent.h>
 #include <Settings.h>
+#include <WindowsKeyboardCodes.h>
 
 #include <FL/Fl.H>
 
@@ -34,7 +38,7 @@ FlEditorClient::FlEditorClient(webview *inview) {
 }
 
 void FlEditorClient::pageDestroyed() {
-	notImplemented();
+	delete this;
 }
 
 bool FlEditorClient::shouldDeleteRange(Range*) {
@@ -191,8 +195,146 @@ void FlEditorClient::redo() {
 	notImplemented();
 }
 
-void FlEditorClient::handleKeyboardEvent(KeyboardEvent*) {
-	notImplemented();
+static const unsigned CtrlKey = 1 << 0;
+static const unsigned AltKey = 1 << 1;
+static const unsigned ShiftKey = 1 << 2;
+
+struct KeyDownEntry {
+	unsigned virtualKey;
+	unsigned modifiers;
+	const char* name;
+};
+
+struct KeyPressEntry {
+	unsigned charCode;
+	unsigned modifiers;
+	const char* name;
+};
+
+static const KeyDownEntry keyDownEntries[] = {
+    { VK_LEFT,   0,                  "MoveLeft"                                    },
+    { VK_LEFT,   ShiftKey,           "MoveLeftAndModifySelection"                  },
+    { VK_LEFT,   CtrlKey,            "MoveWordLeft"                                },
+    { VK_LEFT,   CtrlKey | ShiftKey, "MoveWordLeftAndModifySelection"              },
+    { VK_RIGHT,  0,                  "MoveRight"                                   },
+    { VK_RIGHT,  ShiftKey,           "MoveRightAndModifySelection"                 },
+    { VK_RIGHT,  CtrlKey,            "MoveWordRight"                               },
+    { VK_RIGHT,  CtrlKey | ShiftKey, "MoveWordRightAndModifySelection"             },
+    { VK_UP,     0,                  "MoveUp"                                      },
+    { VK_UP,     ShiftKey,           "MoveUpAndModifySelection"                    },
+    { VK_PRIOR,  ShiftKey,           "MovePageUpAndModifySelection"                },
+    { VK_DOWN,   0,                  "MoveDown"                                    },
+    { VK_DOWN,   ShiftKey,           "MoveDownAndModifySelection"                  },
+    { VK_NEXT,   ShiftKey,           "MovePageDownAndModifySelection"              },
+    { VK_PRIOR,  0,                  "MovePageUp"                                  },
+    { VK_NEXT,   0,                  "MovePageDown"                                },
+    { VK_HOME,   0,                  "MoveToBeginningOfLine"                       },
+    { VK_HOME,   ShiftKey,           "MoveToBeginningOfLineAndModifySelection"     },
+    { VK_HOME,   CtrlKey,            "MoveToBeginningOfDocument"                   },
+    { VK_HOME,   CtrlKey | ShiftKey, "MoveToBeginningOfDocumentAndModifySelection" },
+
+    { VK_END,    0,                  "MoveToEndOfLine"                             },
+    { VK_END,    ShiftKey,           "MoveToEndOfLineAndModifySelection"           },
+    { VK_END,    CtrlKey,            "MoveToEndOfDocument"                         },
+    { VK_END,    CtrlKey | ShiftKey, "MoveToEndOfDocumentAndModifySelection"       },
+
+    { VK_BACK,   0,                  "DeleteBackward"                              },
+    { VK_BACK,   ShiftKey,           "DeleteBackward"                              },
+    { VK_DELETE, 0,                  "DeleteForward"                               },
+    { VK_BACK,   CtrlKey,            "DeleteWordBackward"                          },
+    { VK_DELETE, CtrlKey,            "DeleteWordForward"                           },
+
+    { 'B',       CtrlKey,            "ToggleBold"                                  },
+    { 'I',       CtrlKey,            "ToggleItalic"                                },
+
+    { VK_ESCAPE, 0,                  "Cancel"                                      },
+    { VK_TAB,    0,                  "InsertTab"                                   },
+    { VK_TAB,    ShiftKey,           "InsertBacktab"                               },
+    { VK_RETURN, 0,                  "InsertNewline"                               },
+    { VK_RETURN, CtrlKey,            "InsertNewline"                               },
+    { VK_RETURN, AltKey,             "InsertNewline"                               },
+    { VK_RETURN, AltKey | ShiftKey,  "InsertNewline"                               },
+
+    // It's not quite clear whether clipboard shortcuts and Undo/Redo should be handled
+    // in the application or in WebKit. We chose WebKit for now.
+    { 'C',       CtrlKey,            "Copy"                                        },
+    { 'V',       CtrlKey,            "Paste"                                       },
+    { 'X',       CtrlKey,            "Cut"                                         },
+    { 'A',       CtrlKey,            "SelectAll"                                   },
+    { VK_INSERT, CtrlKey,            "Copy"                                        },
+    { VK_DELETE, ShiftKey,           "Cut"                                         },
+    { VK_INSERT, ShiftKey,           "Paste"                                       },
+    { 'Z',       CtrlKey,            "Undo"                                        },
+    { 'Z',       CtrlKey | ShiftKey, "Redo"                                        }
+};
+
+static const KeyPressEntry keyPressEntries[] = {
+    { '\t',   0,                  "InsertTab"                                   },
+    { '\t',   ShiftKey,           "InsertBacktab"                               },
+    { '\r',   0,                  "InsertNewline"                               },
+    { '\r',   CtrlKey,            "InsertNewline"                               },
+    { '\r',   AltKey,             "InsertNewline"                               },
+    { '\r',   AltKey | ShiftKey,  "InsertNewline"                               }
+};
+
+static const char* interpretKeyEvent(const KeyboardEvent* event)
+{
+	ASSERT(event->type() == eventNames().keydownEvent || event->type() == eventNames().keypressEvent);
+
+	static HashMap<int, const char*>* keyDownCommandsMap = 0;
+	static HashMap<int, const char*>* keyPressCommandsMap = 0;
+
+	if (!keyDownCommandsMap) {
+		keyDownCommandsMap = new HashMap<int, const char*>;
+		keyPressCommandsMap = new HashMap<int, const char*>;
+
+		for (size_t i = 0; i < WTF_ARRAY_LENGTH(keyDownEntries); ++i)
+			keyDownCommandsMap->set(keyDownEntries[i].modifiers << 16 | keyDownEntries[i].virtualKey, keyDownEntries[i].name);
+
+		for (size_t i = 0; i < WTF_ARRAY_LENGTH(keyPressEntries); ++i)
+			keyPressCommandsMap->set(keyPressEntries[i].modifiers << 16 | keyPressEntries[i].charCode, keyPressEntries[i].name);
+	}
+
+	unsigned modifiers = 0;
+	if (event->shiftKey())
+		modifiers |= ShiftKey;
+	if (event->altKey())
+		modifiers |= AltKey;
+	if (event->ctrlKey())
+		modifiers |= CtrlKey;
+
+	if (event->type() == eventNames().keydownEvent) {
+		int mapKey = modifiers << 16 | event->keyCode();
+		return mapKey ? keyDownCommandsMap->get(mapKey) : 0;
+	}
+
+	int mapKey = modifiers << 16 | event->charCode();
+	return mapKey ? keyPressCommandsMap->get(mapKey) : 0;
+}
+
+
+void FlEditorClient::handleKeyboardEvent(KeyboardEvent *ev) {
+	const PlatformKeyboardEvent * const pev = ev->keyEvent();
+	if (!pev)
+		return;
+
+	Node* node = ev->target()->toNode();
+	Frame* frame = node->document().frame();
+	Editor::Command command = frame->editor().command(interpretKeyEvent(ev));
+
+	if (pev->type() == PlatformEvent::RawKeyDown) {
+		if (!command.isTextInsertion() && command.execute(ev))
+			ev->setDefaultHandled();
+		return;
+	}
+
+	command.execute(ev);
+
+	if (pev->ctrlKey() || pev->altKey() || ev->charCode() < ' ')
+		return;
+
+	frame->editor().insertText(pev->text(), ev);
+	ev->setDefaultHandled();
 }
 
 void FlEditorClient::handleInputMethodKeydown(KeyboardEvent*) {
