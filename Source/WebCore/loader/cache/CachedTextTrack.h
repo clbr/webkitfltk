e44f0d315c7573bfb75a3b468b04d18c1294778b
commit-queue@webkit.org <commit-queue@webkit.org>
2014-11-03 22:27:59 +0000

    Unreviewed, rolling out r175406, r175413, and r175423. https://bugs.webkit.org/show_bug.cgi?id=138327
    Multipart tests are still broken (Requested by ap on #webkit).
    
    Reverted changesets:
    
    "Eliminate ResourceBuffer and use SharedBuffer directly
    instead"
    https://bugs.webkit.org/show_bug.cgi?id=138174
    http://trac.webkit.org/changeset/175406
    
    "Unreviewed, iOS build fix since 175406."
    http://trac.webkit.org/changeset/175413
    
    "Fix assertion in CachedResource::addDataBuffer"
    http://trac.webkit.org/changeset/175423
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@175491 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/cache/CachedTextTrack.h
+++ Source/WebCore/loader/cache/CachedTextTrack.h
@@ -36,13 +36,12 @@ namespace WebCore {
 class CachedTextTrack final : public CachedResource {
 public:
     CachedTextTrack(const ResourceRequest&, SessionID);
+    virtual ~CachedTextTrack();
 
 private:
     virtual bool mayTryReplaceEncodedData() const override { return true; }
-    virtual void addDataBuffer(SharedBuffer&) override;
-    virtual void finishLoading(SharedBuffer*) override;
-
-    void updateData(SharedBuffer*);
+    virtual void addDataBuffer(ResourceBuffer*) override;
+    virtual void finishLoading(ResourceBuffer*) override;
 };
 
 } // namespace WebCore
