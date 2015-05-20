b36f6632212383116de7ad8e6addaf33802179a7
Lauri Kasanen <curaga@operamail.com>
2015-05-20 19:46:56 +0300

    Backport e9e797675e6352cd9fc9c65bafb7a13942da189a
    REGRESSION(r152313): Inline-block element doesn't wrap properly

--- Source/WebCore/rendering/line/BreakingContextInlineHeaders.h
+++ Source/WebCore/rendering/line/BreakingContextInlineHeaders.h
@@ -936,7 +936,7 @@ inline bool BreakingContext::canBreakAtThisPosition()
         return false;
 
     // Avoid breaking before empty inlines.
-    if (m_nextObject && m_nextObject->isRenderInline() && isEmptyInline(toRenderInline(*m_nextObject)))
+    if (m_nextObject && !m_current.renderer()->isReplaced() && m_nextObject->isRenderInline() && isEmptyInline(toRenderInline(*m_nextObject)))
         return false;
 
     // Return early if we autowrap and the current character is a space as we will always want to break at such a position.
